3ac307854eaec120e384456af508f07d78fdfce9
Adam Nowland <acnowland@gmail.com>
2023-02-13 16:56:51 -0800

    Initial Commit

---
+++ flagPicker/flagPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  flagPickerApp.swift
+//  flagPicker
+//
+//  Created by Adam Nowland on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct flagPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
