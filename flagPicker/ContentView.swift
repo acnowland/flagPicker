1e415f39952a736c14422558d39d97b06be16251
Adam Nowland <acnowland@gmail.com>
2023-02-13 23:46:35 -0800

    basic design

--- flagPicker/ContentView.swift
+++ flagPicker/ContentView.swift
@@ -8,29 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var currentScore = 0
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    func flagTapped (_ number: Int) {
+        if(number == correctAnswer){
+            scoreTitle = "Correct"
+            currentScore += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func resetGame () {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack (spacing: 30) {
                 VStack {
                     Text("Tap the flag of: ")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle)
                 }
                 
                 ForEach(0..<3) { number in
                     Button {
-                        // flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(radius: 10)
                     }
                 }
+                VStack {
+                    Text("Score: \(currentScore)")
+                }
             }
+        }
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button ("Continue", action: resetGame)
+        } message: {
+            Text("Your score is \(currentScore)")
         }
     }
 }
